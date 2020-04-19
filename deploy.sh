d8d7414344c6d2a6eccc22c11aef59bbfbc6c1c5
Benji <dev@fortrieb.org>
2020-04-19 17:11:52 +0200

    deploy script uses current working
    directory instead of absolute path

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-cp j-lawyer-server/dist/j-lawyer-server.ear ~/bin/wildfly-16.0.0.Final//standalone/deployments/j-lawyer-server.ear
+CURRENT=${PWD}
+cp j-lawyer-server/dist/j-lawyer-server.ear $CURRENT/wildfly-16.0.0.Final/standalone/deployments/j-lawyer-server.ear
